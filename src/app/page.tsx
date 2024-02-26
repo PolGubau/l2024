4335198e24154cad9eeba40d238ceb1698289271
Pol Gubau <gubaupol@gmail.com>
2024-02-26 12:23:01 +0100

    Delete unused files and update configuration

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MapBcn from "@/components/MapBcn";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 h-screen w-screen">
-      <MapBcn />
-    </main>
-  );
-}
