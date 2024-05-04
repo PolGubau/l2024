56f53431f029828551e70b52a71353ac681de8e6
Pol Gubau Amores <gubaupol@gmail.com>
2024-05-05 01:56:52 +0200

    Refactor code to improve performance

--- src/types/lines.ts
+++ src/types/lines.ts
@@ -2,21 +2,3 @@ export interface Line {
   type: string;
   features: GeoJSON.Feature;
 }
-
-export interface Feature {
-  type: string;
-  properties: Properties;
-  geometry: Geometry;
-}
-
-export interface Properties {
-  name: string;
-  type: string;
-  time: string;
-  coordTimes: string[];
-}
-
-export interface Geometry {
-  type: string;
-  coordinates: number[][];
-}
--- src/types/stops.ts
+++ src/types/stops.ts
@@ -16,17 +16,11 @@ export interface Geometry {
 
 export interface Properties {
   CODI_CAPA: string;
-  CAPA_GENERICA: string;
-  NOM_CAPA: string;
   ED50_COORD_X?: number;
   ED50_COORD_Y?: number;
-  ETRS89_COORD_X?: number;
-  ETRS89_COORD_Y?: number;
   EQUIPAMENT: string;
   DISTRICTE?: number;
   BARRI?: number;
   NOM_DISTRICTE: string;
   NOM_BARRI: string;
-  ADRECA: string;
-  TELEFON: string;
 }
